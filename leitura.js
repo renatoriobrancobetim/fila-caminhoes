5b828b0ba5cd0f297f1a1317f62ff15591ebe0f1
renatoriobrancobetim <renato.riobrancobetim@gmail.com>
2025-12-30 15:59:53 -0300

    Update leitura.js

--- leitura.js
+++ leitura.js
@@ -1,1 +1,24 @@
+function renderizar() {
+  const fila = JSON.parse(localStorage.getItem("fila")) || [];
+  const tbody = document.getElementById("fila");
+
+  tbody.innerHTML = "";
+
+  fila.forEach((item, index) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${index + 1}</td>
+      <td>${item.placa}</td>
+      <td>${item.compartimento}</td>
+      <td>${item.data}</td>
+      <td>${item.hora}</td>
+    `;
+    tbody.appendChild(tr);
+  });
+}
+
+renderizar();
+
+// Atualiza automaticamente a cada 5 segundos
+setInterval(renderizar, 5000);
 
