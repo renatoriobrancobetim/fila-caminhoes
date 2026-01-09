bcab7beb57830edb7c2c15dd081b6bc10ea3f5c3
renatoriobrancobetim <renato.riobrancobetim@gmail.com>
2026-01-09 11:14:31 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "motoristas-pwa-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "motoristas.html",
+        "manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
