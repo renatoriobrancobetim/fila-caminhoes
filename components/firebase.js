32d7b28eabd9b34829f01c704931638e3831378d
renatoriobrancobetim <renato.riobrancobetim@gmail.com>
2026-02-27 15:39:59 -0300

    Create firebase.js

---
+++ components/firebase.js
@@ -0,0 +1,16 @@
+// components/firebase.js
+
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey:"AIzaSyCKdfOvWehaJcuK6uX5JXxc7YdN26h8sXY",
+  authDomain:"tabela-frete-rio-branco.firebaseapp.com",
+  projectId:"tabela-frete-rio-branco"
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
