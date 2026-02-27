9378f1c51c3d6cb0fd5f8407ff3c6c5fc34539bb
renatoriobrancobetim <renato.riobrancobetim@gmail.com>
2026-02-27 15:40:53 -0300

    Update security.js

--- components/security.js
+++ components/security.js
@@ -1,18 +1,8 @@
 // components/security.js
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey:"AIzaSyCKdfOvWehaJcuK6uX5JXxc7YdN26h8sXY",
-  authDomain:"tabela-frete-rio-branco.firebaseapp.com",
-  projectId:"tabela-frete-rio-branco"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+import { auth, db } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 onAuthStateChanged(auth, async (user)=>{
 
@@ -33,15 +23,11 @@ onAuthStateChanged(auth, async (user)=>{
   const requiredMenu = document.body.dataset.menu;
   const requiredNivel = document.body.dataset.nivel;
 
-  // 🔴 Verificação por nível
-  if(requiredNivel){
-    if(dados.nivel < parseInt(requiredNivel)){
-      window.location.href="painel.html";
-      return;
-    }
+  if(requiredNivel && dados.nivel < parseInt(requiredNivel)){
+    window.location.href="painel.html";
+    return;
   }
 
-  // 🟡 Verificação por menu
   if(requiredMenu){
     if(dados.nivel !== 4 && !dados.menus?.[requiredMenu]){
       window.location.href="painel.html";
